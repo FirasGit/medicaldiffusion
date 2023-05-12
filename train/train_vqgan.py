0e997a9f801dec9882476efcc24dabc8692a4b31
dmnk1308 <dominikbe@aol.com>
2023-05-12 15:06:34 +0200

    append sys path

--- train/train_vqgan.py
+++ train/train_vqgan.py
@@ -1,6 +1,8 @@
 "Adapted from https://github.com/SongweiGe/TATS"
 
 import os
+import sys
+sys.path.append(os.path.split(os.getcwd())[0])
 import pytorch_lightning as pl
 from pytorch_lightning.callbacks import ModelCheckpoint
 from torch.utils.data import DataLoader
